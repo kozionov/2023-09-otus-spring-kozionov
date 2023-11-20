f91fc95ebc9c1e7712106e4fdf5a9fce7e795d89
Sergey <kozionov@rambler.ru>
2023-11-20 08:47:05 +0500

    Доработка

--- hw07/src/main/java/ru/otus/hw/repositories/BookRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -1,10 +1,13 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import ru.otus.hw.models.Book;
 
 import java.util.List;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
+
+    @EntityGraph(attributePaths = {"author", "genres"})
     List<Book> findAll();
 }

1e1033703543c6641fce4ccbd618265674c28ac8
Sergey <kozionov@rambler.ru>
2023-12-13 07:39:12 +0500

    Удаление работает

--- hw09/src/main/java/ru/otus/hw/controller/BookController.java
+++ hw09/src/main/java/ru/otus/hw/controller/BookController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;

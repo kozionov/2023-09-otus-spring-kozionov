96a864190583c14b1bf5d55933bbbdc9531521c6
Sergey <kozionov@rambler.ru>
2024-01-19 09:18:58 +0500

    Заложил код прошлого дз

---
+++ hw13/src/main/java/ru/otus/hw/controller/BookController.java
@@ -0,0 +1,69 @@
+package ru.otus.hw.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.otus.hw.dto.BookCreateDto;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookUpdateDto;
+import ru.otus.hw.services.AuthorService;
+import ru.otus.hw.services.BookService;
+import ru.otus.hw.services.GenreService;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService service;
+
+    private final GenreService genreService;
+
+    private final AuthorService authorService;
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        List<BookDto> books = service.findAll();
+        model.addAttribute("books", books);
+        return "main";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("genres", genreService.findAll());
+        model.addAttribute("authors", authorService.findAll());
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createBook(@Valid BookCreateDto bookCreateDto) {
+        service.insert(bookCreateDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/edit")
+    public String editPage(@RequestParam("id") long id, Model model) {
+        BookDto book = service.findById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("genres", genreService.findAll());
+        model.addAttribute("authors", authorService.findAll());
+        return "edit";
+    }
+
+    @PostMapping("/edit")
+    public String editBook(@Valid BookUpdateDto bookUpdateDto) {
+        service.update(bookUpdateDto);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String delete(@RequestParam("id") long id) {
+        service.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ hw13/src/main/java/ru/otus/hw/services/BookService.java
@@ -0,0 +1,24 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.dto.BookCreateDto;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookUpdateDto;
+
+import java.util.List;
+
+public interface BookService {
+    BookDto findById(long id);
+
+    List<BookDto> findAll();
+
+    BookDto insert(String title, long authorId, List<Long> genresIds);
+
+    BookDto update(long id, String title, long authorId, List<Long> genresIds);
+
+    void deleteById(long id);
+
+    BookDto insert(BookCreateDto bookCreateDto);
+
+    BookDto update(BookUpdateDto bookUpdateDto);
+
+}
